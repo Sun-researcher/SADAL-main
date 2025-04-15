945cbbb335030bf0433661732310e7e3d0435923
Sun-researcher <84301317+Sun-researcher@users.noreply.github.com>
2025-04-15 16:21:33 +0800

    Update loss.py

--- models/loss.py
+++ models/loss.py
@@ -12,10 +12,27 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 
 
-def adv(features, ad_net): #域判别损失
+def adv(features, ad_net): 
     ad_out = ad_net(features)
     batch_size = ad_out.size(0) // 2
     dc_target = torch.from_numpy(np.array([[1]] * batch_size + [[0]] * batch_size)).float().to(features.device)
     return torch.nn.BCELoss()(ad_out, dc_target)
 
+def entropy(input_): 
+    bs = input_.size(0)
+    epsilon = 1e-10
+    entropy = -input_ * torch.log2(input_ + epsilon)
+    entropy = torch.sum(entropy, dim=1)
+    return entropy
+
+def im(outputs_test, gent=True):
+    epsilon = 1e-10
+    softmax_out = nn.Softmax(dim=1)(outputs_test)
+    entropy_loss = torch.mean(entropy(softmax_out))
+    if gent:
+        msoftmax = softmax_out.mean(dim=0)
+        gentropy_loss = torch.sum(-msoftmax * torch.log2(msoftmax + epsilon))
+        entropy_loss -= gentropy_loss
+    im_loss = entropy_loss * 1.0
+    return im_loss
 
